391f4a585723f743e41333df070dcf1595de070c
kangod <kangod.yan@gmail.com>
2023-11-26 14:45:07 +0800

    fix: 1、修改项目模块点击项目打开新标签

--- components/Card.tsx
+++ components/Card.tsx
@@ -18,7 +18,7 @@ const Card = ({ title, description, imgSrc, href, techStack }) => {
   }
 
   return (
-    <a href={href} className="max-w-lg">
+    <a target="_blank" rel="noopener noreferrer" href={href} className="max-w-lg">
       <div className="flex flex-col h-full overflow-hidden rounded-xl border-2 p-[6px] border-zinc-300 hover:border-black dark:border-zinc-700 hover:dark:border-white/90">
         {imgSrc && (
           <div className="flex-shrink-0 object-none">
