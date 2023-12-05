f1d0bf9b740f16b1b0f2922147ce0302624a7e7e
kangod <kangod.yan@gmail.com>
2023-12-05 18:13:37 +0800

    fix: 1、增加 blog 分页的 app 下页面

---
+++ app/blog/page/[page]/page.tsx
@@ -0,0 +1,37 @@
+import ListLayout from '@/layouts/ListLayoutWithTags'
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
+import { allBlogs } from 'contentlayer/generated'
+import { countListArticleTag } from 'http/services/api'
+
+const POSTS_PER_PAGE = 5
+
+export const generateStaticParams = async () => {
+  const totalPages = Math.ceil(allBlogs.length / POSTS_PER_PAGE)
+  const paths = Array.from({ length: totalPages }, (_, i) => ({ page: (i + 1).toString() }))
+
+  return paths
+}
+
+export default async function Page({ params }: { params: { page: string } }) {
+  const posts = allCoreContent(sortPosts(allBlogs))
+  const pageNumber = parseInt(params.page as string)
+  const initialDisplayPosts = posts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  )
+  const pagination = {
+    currentPage: pageNumber,
+    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+  }
+  // 异步查询标签数量集合，传给ListLayout
+  const tagCountList = await countListArticleTag()
+  return (
+    <ListLayout
+      tagCountList={tagCountList}
+      posts={posts}
+      initialDisplayPosts={initialDisplayPosts}
+      pagination={pagination}
+      title="全部文章"
+    />
+  )
+}
