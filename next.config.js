b2631d8d2d08b8bd354dbe6c11d1d6b6dae9fccc
kangod <kangod.yan@gmail.com>
2024-05-18 10:07:37 +0800

    asset: 文章内的图片上传

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const ContentSecurityPolicy = `
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
-  media-src github.com raw.githubusercontent.com;
+  media-src *;
   connect-src *;
   font-src 'self';
   frame-src giscus.app codepen.io ;
